af28f0759a8e282df32fdb91bd62ad785eeffef5
Arthur Nascimento Pereira <arthurnapereira@gmail.com>
2026-05-12 21:23:09 -0300

    Adicionando modulos e async functions

---
+++ ProgWeb_1/js/modulo1.js
@@ -0,0 +1,8 @@
+export var vProfessor_modulo = {
+    nome: 'João', 
+    idade: 40, 
+    departamento: 'DSC'};
+
+export function olaMundo(){
+    console.log('Ola Mundo');
+}
---
+++ ProgWeb_1/js/modulo2.js
@@ -0,0 +1,11 @@
+function olaMundo2(){
+    console.log('Ola Mundo 2');
+}
+
+const cALuno = {
+    nome: "Arthur",
+    idade: 19,
+    curso: "BCC"
+}
+
+export {olaMundo2, cALuno};
